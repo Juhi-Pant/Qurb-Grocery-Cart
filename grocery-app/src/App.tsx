e3395ad73238dc80957d6bb9f6c64cfeb7930129
Juhi-Pant <pantjuhi2009@gmail.com>
2025-05-19 00:56:14 +0530

    error

--- grocery-app/src/App.tsx
+++ grocery-app/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as  Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import NavBar from './components/NavBar';
